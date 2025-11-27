df8aff5adb1ac9411a28df5a5e56866cb5d8fa67
Davit Kutsia <132742480+DavitKutsia@users.noreply.github.com>
2025-11-27 23:13:09 +0400

    Update user.model.js

--- models/user.model.js
+++ models/user.model.js
@@ -1,32 +1,30 @@
-const e = require("express");
 const { default: mongoose } = require("mongoose");
 
-const userModel = new mongoose.Schema({
+const userModel = new mongoose.Schema(
+  {
     name: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
     },
     email: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
     },
     password: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
     },
     role: {
-        type: String,
-        enum: ['user', 'admin'],
-        default: 'user'
+      type: String,
+      enum: ["user", "admin"],
+      default: "user",
     },
     subscriptionActive: {
-        type: Boolean,
-        default: false,
+      type: Boolean,
+      default: false,
     },
   },
   { timestamps: true }
 );
-
-}, {timestamps: true});
 
 module.exports = mongoose.model("User", userModel);
