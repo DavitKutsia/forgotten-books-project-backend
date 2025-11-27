90056af90e793d62f13881e4ff515570cc46273a
Davit Kutsia <132742480+DavitKutsia@users.noreply.github.com>
2025-11-27 16:14:20 +0400

    Refactor user model schema formatting

--- models/user.model.js
+++ models/user.model.js
@@ -1,36 +1,32 @@
+const e = require("express");
 const { default: mongoose } = require("mongoose");
 
-const userModel = new mongoose.Schema(
-  {
+const userModel = new mongoose.Schema({
     name: {
-      type: String,
-      required: true,
+        type: String,
+        required: true,
     },
     email: {
-      type: String,
-      required: true,
+        type: String,
+        required: true,
     },
     password: {
-      type: String,
-      required: true,
+        type: String,
+        required: true,
     },
     role: {
-      type: String,
-      enum: ["user", "admin"],
-      default: "user",
+        type: String,
+        enum: ['user', 'admin'],
+        default: 'user'
     },
     subscriptionActive: {
-      type: Boolean,
-      default: false,
-    },
-    stripeCustomerId: {
-      type: String,
-    },
-    stripeSubscriptionId: {
-      type: String,
+        type: Boolean,
+        default: false,
     },
   },
   { timestamps: true }
 );
+
+}, {timestamps: true});
 
 module.exports = mongoose.model("User", userModel);
