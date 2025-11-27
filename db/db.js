966268697dddcd7403e31691d110878adc2835b6
Davit Kutsia <132742480+DavitKutsia@users.noreply.github.com>
2025-11-28 00:01:57 +0400

    Update db.js

--- db/db.js
+++ db/db.js
@@ -1,14 +1,17 @@
-    const { default: mongoose } = require("mongoose")
-    require('dotenv').config()
+const { default: mongoose } = require("mongoose");
+require('dotenv').config();
 
-    const connectToDb = async () => {
-        try{
-            await mongoose.connect(process.env.MONGO_URL)
-            console.log('Connected Successfully')
-        }catch(e){
-            console.log(e)
-        }
-    }
+const connectToDb = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('DB Connected Successfully');
+  } catch (e) {
+    console.error('DB Connection Error:', e);
+    throw e; 
+  }
+};
 
-
-    module.exports = connectToDb
+module.exports = connectToDb;
